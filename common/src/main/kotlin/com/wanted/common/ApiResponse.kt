24951f10fd0eab32a2e9e0105ee1df2d8a529aba
harok <dorianharok@github.com>
2024-09-05 18:47:34 +0900

    feat: 공통 response 구현 (#2)

---
+++ common/src/main/kotlin/com/wanted/common/ApiResponse.kt
@@ -0,0 +1,22 @@
+package com.wanted.common
+
+data class ApiResponse<T>(
+    val status: ResultType,
+    val message: String? = null,
+    val data: T? = null
+) {
+    companion object {
+        fun <T> success(message: String? = null, data: T? = null): ApiResponse<T> =
+            ApiResponse(ResultType.SUCCESS, message, data)
+
+        fun <T> success(data: T): ApiResponse<T> =
+            ApiResponse(ResultType.SUCCESS, data = data)
+
+        fun error(message: String): ApiResponse<Nothing> =
+            ApiResponse(ResultType.ERROR, message)
+    }
+}
+
+enum class ResultType {
+    SUCCESS, ERROR
+}
