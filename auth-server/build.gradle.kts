4f3bdfafb8047804bacda4f60b42e4659e5f99ac
harok <dorianharok@github.com>
2024-09-08 00:42:17 +0900

    refactor(auth): JBCrypt, TestContainers 의존성 추가

--- auth-server/build.gradle.kts
+++ auth-server/build.gradle.kts
@@ -22,12 +22,18 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 
 	// jwt
 	implementation("io.jsonwebtoken:jjwt-api:0.12.5")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
 
-	// swagger
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+	// JBCrypt
+	implementation("org.mindrot:jbcrypt:0.4")
+
+	// test containers
+	testImplementation("org.testcontainers:testcontainers:1.20.1")
+	testImplementation("org.testcontainers:junit-jupiter:1.20.1")
+	testImplementation("org.testcontainers:mariadb:1.19.3")
 }
