4f1734c15863b3cf0255dd60bb581acf85a78982
harok <dorianharok@github.com>
2024-09-08 00:42:27 +0900

    refactor(common): swagger 의존성 추가

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,4 +1,8 @@
 dependencies {
+    // grpc
     implementation("io.grpc:grpc-protobuf:1.66.0")
     implementation("io.grpc:grpc-kotlin-stub:1.4.1")
+
+    // swagger
+    api("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
 }
