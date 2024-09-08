53ba54faa321de3cbffd1a1c2fc3b33585d94b0c
harok <dorianharok@github.com>
2024-09-09 03:15:55 +0900

    chore(resource): test container 의존성 추가

--- resource-server/build.gradle.kts
+++ resource-server/build.gradle.kts
@@ -23,4 +23,9 @@ dependencies {
     testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // test containers
+    testImplementation("org.testcontainers:testcontainers:1.20.1")
+    testImplementation("org.testcontainers:junit-jupiter:1.20.1")
+    testImplementation("org.testcontainers:mariadb:1.19.3")
 }
