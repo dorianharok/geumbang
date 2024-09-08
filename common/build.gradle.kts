3325d4a6aa0829d5cb67f3ad5a5c8e59c19d3fb4
harok <dorianharok@github.com>
2024-09-08 22:03:16 +0900

    chore(common): grpc 의존성 추가

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,8 +1,43 @@
+import com.google.protobuf.gradle.id
+
+plugins {
+    kotlin("jvm")
+    id("com.google.protobuf") version "0.9.4"
+}
+
 dependencies {
     // grpc
-    implementation("io.grpc:grpc-protobuf:1.66.0")
-    implementation("io.grpc:grpc-kotlin-stub:1.4.1")
+    api("io.grpc:grpc-protobuf:1.66.0")
+    api("io.grpc:grpc-kotlin-stub:1.4.1")
+    api("io.grpc:grpc-netty-shaded:1.66.0")
+    api("io.grpc:grpc-core:1.66.0")
+    api("com.google.protobuf:protobuf-kotlin:3.25.1")
 
     // swagger
     api("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.25.1"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.66.0"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:1.4.1:jdk8@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+            it.builtins {
+                id("kotlin")
+            }
+        }
+    }
 }
