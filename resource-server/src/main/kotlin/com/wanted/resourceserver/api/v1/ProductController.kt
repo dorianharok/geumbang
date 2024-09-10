febd0fc92b948275d3cb01efa525f651ed21f3e5
harok <dorianharok@github.com>
2024-09-10 13:47:27 +0900

    fix(resource): 상품 조회 API method 추가

--- resource-server/src/main/kotlin/com/wanted/resourceserver/api/v1/ProductController.kt
+++ resource-server/src/main/kotlin/com/wanted/resourceserver/api/v1/ProductController.kt
@@ -3,12 +3,15 @@ package com.wanted.resourceserver.api.v1
 import com.wanted.common.ApiResponse
 import com.wanted.resourceserver.api.v1.response.ProductResponse
 import com.wanted.resourceserver.domain.product.ProductService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class ProductController(
     private val productService: ProductService
 ): ProductApi {
+
+    @GetMapping("/api/v1/products")
     override fun getProducts(): ApiResponse<List<ProductResponse>> {
         val products = productService.readAll().map { ProductResponse(it) }
 
